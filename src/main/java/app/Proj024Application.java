1c8497c3d43fe63d0c26b3d34a408711dd8248e5
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2023-09-05 22:28:03 -0300

    Adiciona estrutura do projeto

---
+++ src/main/java/app/Proj024Application.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proj024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proj024Application.class, args);
+	}
+
+}
