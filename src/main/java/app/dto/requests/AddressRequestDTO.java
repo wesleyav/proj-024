7d5f9fc6dfdf3bd508e928f2669a03cfbc6e7bc6
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2023-09-06 23:46:33 -0300

    Adiciona classe AddressRequestDTO

---
+++ src/main/java/app/dto/requests/AddressRequestDTO.java
@@ -0,0 +1,69 @@
+package app.dto.requests;
+
+import app.entities.Address;
+
+public class AddressRequestDTO {
+
+	private String address;
+
+	private String address2;
+
+	private String district;
+
+	private String postalCode;
+
+	private String phone;
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getAddress2() {
+		return address2;
+	}
+
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+
+	public String getDistrict() {
+		return district;
+	}
+
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public Address convertToDTO(AddressRequestDTO dto) {
+		Address address = new Address();
+
+		address.setAddress(dto.getAddress());
+		address.setAddress2(dto.getAddress2());
+		address.setDistrict(dto.getDistrict());
+		address.setPostalCode(dto.getPostalCode());
+		address.setPhone(dto.getPhone());
+
+		return address;
+	}
+
+}
