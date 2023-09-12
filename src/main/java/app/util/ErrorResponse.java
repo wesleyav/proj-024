714ae0e40ec62a438152dfa7fc74d67e7f34e865
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2023-09-12 14:34:12 -0300

    Adiciona classe utilitária ErrorResponse

---
+++ src/main/java/app/util/ErrorResponse.java
@@ -0,0 +1,30 @@
+package app.util;
+
+import org.springframework.http.HttpStatus;
+
+public class ErrorResponse {
+
+	private HttpStatus status;
+	private String message;
+
+	public ErrorResponse(HttpStatus status, String message) {
+		this.status = status;
+		this.message = message;
+	}
+
+	public HttpStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(HttpStatus status) {
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
