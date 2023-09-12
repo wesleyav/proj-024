b1c4a7d941c2cc4ebe876872bc1eb5539dec6df3
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2023-09-12 14:26:17 -0300

    Adiciona métodos de busca na interface AddressRepository

--- src/main/java/app/repositories/AddressRepository.java
+++ src/main/java/app/repositories/AddressRepository.java
@@ -1,9 +1,15 @@
 package app.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import app.entities.Address;
 
 public interface AddressRepository extends JpaRepository<Address, Long> {
+
+	boolean existsByAddress(String address);
+
+	@Query("SELECT a FROM Address a WHERE a.address = :address")
+	Address findByAddress(String address);
 
 }
